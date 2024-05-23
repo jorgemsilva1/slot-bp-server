acdef96e37572c0b754fba1dd51429830ce66881
Jorge Silva <jorgesilva@Jorges-MBP.home>
2024-05-23 23:10:51 +0100

    added sib after creation of play

--- src/api/play/services/play.js
+++ src/api/play/services/play.js
@@ -21,10 +21,17 @@ module.exports = createCoreService("api::play.play", ({ strapi }) => ({
       throw new Error(`Player with email ${playerEmail} not found`);
     }
 
+    let award;
+
+    if (ctx[0].data.prize_id) {
+      award = await strapi.query("api::award.award").findOne({
+        where: { id: ctx[0].data.prize_id },
+      });
+    }
+
     const clients = strapi.wss.clients;
     for (const client of clients) {
       if (client.readyState === WebSocket.OPEN && playerEmail === client.id) {
-        let award;
         if (ctx[0].data.prize_id) {
           award = await strapi.query("api::award.award").findOne({
             where: { id: ctx[0].data.prize_id },
@@ -47,48 +54,52 @@ module.exports = createCoreService("api::play.play", ({ strapi }) => ({
             },
           });
 
-          if (award){
-            const SibApiV3Sdk = require('@getbrevo/brevo');
-
-            let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-            let apiKey = apiInstance.authentications['apiKey'];
-            apiKey.apiKey = process.env.BREVO_KEY;
-
-            let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-
-            sendSmtpEmail.subject = "Prémio Bacana PLAY!";
-            sendSmtpEmail.htmlContent = "<html><body><h1>Viva! Acabaste de receber o seguinte premio: {{params.prize}}</h1></body></html>";
-            sendSmtpEmail.sender = {"email":"guilherme@dvagar.cc","name":"Bacana Play"};
-            sendSmtpEmail.to = [{"email":client.id}];
-            sendSmtpEmail.params = {"prize":award.name};
-
-            apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
-
-            }, function(error) {
-              console.error(error);
-            });
-          }
-
           if (plays?.length >= 4 || plays?.find((play) => play.won)) {
             if (!award) {
               client.send("limit");
             } else {
-              // setTimeout(() => {
-              //   client.send("limit");
-              // }, 5000);
+              setTimeout(() => {
+                client.send("limit");
+              }, 5000);
             }
           }
 
-          // if (ctx[0].data.won) {
-          //   setTimeout(() => {
-          //     client.send("limit");
-          //   }, 5000);
-          // }
+          if (ctx[0].data.won) {
+            setTimeout(() => {
+              client.send("limit");
+            }, 5000);
+          }
         }
       }
     }
 
-    return await super.create(...ctx);
+    const res = await super.create(...ctx);
+    if (award) {
+      const SibApiV3Sdk = require("@getbrevo/brevo");
+
+      let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+      let apiKey = apiInstance.authentications["apiKey"];
+      apiKey.apiKey = process.env.BREVO_KEY;
+
+      let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+      sendSmtpEmail.subject = "Prémio Bacana PLAY!";
+      sendSmtpEmail.templateId = 12;
+      sendSmtpEmail.sender = {
+        email: "guilherme@dvagar.cc",
+        name: "Bacana Play",
+      };
+      sendSmtpEmail.to = [{ email: playerEmail }];
+      sendSmtpEmail.params = { prize_name: award.name, prize_id: res.id };
+
+      apiInstance.sendTransacEmail(sendSmtpEmail).then(
+        function (data) {},
+        function (error) {
+          console.error(error);
+        }
+      );
+    }
+    return res;
   },
 }));
